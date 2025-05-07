e4f626fe818e01bcf9f260c329785f7e32dbe86f
eliBrank <ezb.business@pm.me>
2025-05-07 16:01:05 -0500

    connect main.jsx with App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
-import "./index.css";
+import './index.css';
+import ReactDOM from 'react-dom/client';
+import App from "./App";
 
-// Render the App component to the root element
+const rootDiv = document.getElementById('root');
+const root = ReactDOM.createRoot(rootDiv);
+
+root.render(<App />);
